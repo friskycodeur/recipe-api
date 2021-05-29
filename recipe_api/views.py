0bc6b64a31bed714db15ff9e11c1b2660cf15a89
Prateek <friskycodeur@gmail.com>
2021-05-29 15:07:10 +0530

    created views

--- recipe_api/views.py
+++ recipe_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, resolve_url
+from django.http import HttpResponse
 # Create your views here.
+from .models import Recipe,Tag,ingredient
+from rest_framework.response import Response
+from .serializers import RecipeSerializer,TagSerializer,ingredientSerializer
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+
+class TagViewset(viewsets.ModelViewSet):
+    serializer_class=TagSerializer
+    queryset=Tag.objects.all()
+
+class RecipeViewset(viewsets.ModelViewSet):
+    serializer_class=RecipeSerializer
+    queryset=Recipe.objects.all()
+
+class ingredientViewset(viewsets.ModelViewSet):
+    serializer_class=ingredientSerializer
+    queryset=ingredient.objects.all()
